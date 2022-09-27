c471cc4dbfe7432341f6cf67abec4feff84b196e
gameman2233 <kpeloquin322@gmail.com>
2022-09-26 20:17:57 -0400

    test ng

---
+++ js/randompage.js
@@ -0,0 +1,2 @@
+var arrayofsites = ["www.lifeisdrain.wtf/miku", "www.lifeisdrain.wtf/foosball", "www.lifeisdrain.wtf/twisted", ];
+var randomsite = arrayofsites[Math.floor(Math.random() * arrayofsites.length)];
