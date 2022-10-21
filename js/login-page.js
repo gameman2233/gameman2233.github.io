9ed5e3fab3737059b23350ed6e73dc907233fcdc
Ethan Cardoso <ethancardoso1@outlook.com>
2022-10-20 20:32:07 -0400

    fix

--- js/login-page.js
+++ js/login-page.js
@@ -6,9 +6,9 @@ loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const password = loginForm.password.value;
 
-    if (password === "test") {
+    if (password === "ethanisawesome") {
         loginButton.onclick = function() {
-            location.href = "https://www.lifeisdrain.wtf/miku.php";
+            location.href = "https://www.lifeisdrain.wtf/miku.html";
         }
     } else {
         loginErrorMsg.style.opacity = 1;
