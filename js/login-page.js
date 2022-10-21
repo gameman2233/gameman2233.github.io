d40b1587fd12bc72813e8d2796cda00e337888db
Ethan Cardoso <ethancardoso1@outlook.com>
2022-10-20 20:29:26 -0400

    Login Added

---
+++ js/login-page.js
@@ -0,0 +1,17 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const password = loginForm.password.value;
+
+    if (password === "test") {
+        loginButton.onclick = function() {
+            location.href = "https://www.lifeisdrain.wtf/miku.php";
+        }
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
+
--- js/randompage.js
+++
@@ -1,2 +0,0 @@
-var arrayofsites = ["www.lifeisdrain.wtf/miku", "www.lifeisdrain.wtf/foosball", "www.lifeisdrain.wtf/twisted", ];
-var randomsite = arrayofsites[Math.floor(Math.random() * arrayofsites.length)];
